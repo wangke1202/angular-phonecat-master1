054bd071ecca549adc712e841a84b738025a0dfa
wangke1202 <wangke122@gmail.com>
2014-02-03 22:19:31 +0800

    all code checked in
    all code checked in

---
+++ app/js/controllers.js
@@ -0,0 +1,10 @@
+function PhoneListCtrl1($scope) {
+  $scope.phones22 = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
